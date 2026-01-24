7d4b6b0f4d199aaaf63524b3935d0b31193cb512
kmohan321 <kmalphamale@gmail.com>
2026-01-24 18:04:04 +0530

    added ffn_affine

--- common/normalisation.py
+++ common/normalisation.py
@@ -13,13 +13,13 @@ class RMSE_NORM(nn.Module):
   def forward(self, x):
     x = x * torch.rsqrt((x**2).mean(dim=-1, keepdim=True) + self.eps)
     return x  * self.weight
-  
 
 class LAYER_NORM(nn.Module):
-  def __init__(self, hidden_dim, eps):
+  def __init__(self, hidden_dim, eps, elem_affine = True):
     super().__init__()
     
     self.eps = eps
+    self.affine = elem_affine
     self.bias = nn.Parameter(torch.zeros(hidden_dim))
     self.weight = nn.Parameter(torch.ones(hidden_dim))
     
@@ -28,5 +28,8 @@ class LAYER_NORM(nn.Module):
     var = torch.var(x,-1,keepdim=True,unbiased=False)
     
     x = (x-mean)/torch.sqrt((var + self.eps))
-    return x * self.weight + self.bias
+    
+    if self.affine:
+      x = x * self.weight + self.bias
+    return x
 
