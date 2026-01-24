e7355d53834cfbd66d33a0d9cc419197a61300fb
kmohan321 <kmalphamale@gmail.com>
2026-01-24 20:53:54 +0530

    added Mistral Model

--- Mistral/Gqa_sa.py
+++ Mistral/Gqa_sa.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from utils.rope import apply_rope
+from common import apply_rope
 
 class GQA_SA(nn.Module):
   def __init__(self,
@@ -23,33 +23,25 @@ class GQA_SA(nn.Module):
     self.query_groups = self.q_heads // self.kv_heads
     self.scale = self.d_head ** -0.5
     
-  def forward(self, x: torch.Tensor, rope_freq : torch.Tensor = None , mask: torch.Tensor = None):
+  def forward(self, x: torch.Tensor, rope_freq : torch.Tensor, mask: torch.Tensor | None = None):
 
       b,s,d = x.shape
       q = self.wq(x).view(b, s, self.q_heads, self.d_head)
       k = self.wk(x).view(b, s, self.kv_heads, self.d_head)
       v = self.wv(x).view(b, s, self.kv_heads, self.d_head)
       
+      q, k, v = q.transpose(1,2), k.transpose(1,2), v.transpose(1,2)
       q,k = apply_rope(q, rope_freq), apply_rope(k, rope_freq)
-      q,k,v = q.transpose(1,2), k.transpose(1,2), v.transpose(1,2)
       
-      # query -> (b, q_head, s, d_h)
-      #key -> (b, kv_heads, s, d_h)
-      q = q.view(b, self.query_groups, self.kv_heads, s, self.d_head)
-      attn_scores = q @ k.transpose(2,3).unsqueeze(1) #key -> (b,1,kv_heads,d,s)
+      if self.query_groups > 0:
+        k = k.repeat_interleave(self.query_groups, dim = 1)
+        v = v.repeat_interleave(self.query_groups, dim = 1)
+        
+      attn_scores = q @ k.transpose(2,3)
       if mask is not None:
-        attn_scores = attn_scores.masked_fill(mask ==0, -torch.inf)
+        attn_scores = attn_scores + mask
       attn_scores = torch.softmax((attn_scores * self.scale), dim=-1)
       
-      out = (attn_scores @ v.unsqueeze(1)).transpose(1,2).contiguous().view(b,s,d)
+      out = (attn_scores @ v).transpose(1,2).contiguous().view(b,s,d)
       return self.wo(out)
-      
-      
-# x =  torch.randn(2,3,4)
-# attn = GQA_SA(4,2,4,2)
-# i = torch.arange(0,3).view(3,1)
-# j = torch.arange(0,3).view(1,3)
-# mask = (i>=j) & (j >= i-2+1)
-# print(attn(x,mask = mask).shape)
-# print(attn(x))
-      
+            
--- Mistral/mistral.py
+++ Mistral/mistral.py
@@ -1,51 +1,20 @@
 import torch
 import torch.nn as nn
-from Gqa_sa import GQA_SA
-from utils.rope import Frequencies
-
-class RMSE_NORM(nn.Module):
-  def __init__(self,
-               hidden_dim:int,
-               eps:float = 1e-5
-               ):
-    super().__init__()
-    self.eps = eps
-    self.shift = nn.Parameter(torch.ones(hidden_dim))
-
-  def forward(self,x):
-    x = x * torch.rsqrt((x**2).mean(dim=-1, keepdim=True) + self.eps)
-    return x  * self.shift
-
-class FFN(nn.Module):
-  def __init__(self,
-               hidden_dim:int,
-               ffn_multiplier: float
-               ):
-    super().__init__()
-    
-    self.w1 = nn.Linear(hidden_dim,ffn_multiplier*hidden_dim,bias=False)
-    self.w2 = nn.Linear(ffn_multiplier*hidden_dim,hidden_dim,bias=False)
-    self.w3 = nn.Linear(hidden_dim,ffn_multiplier*hidden_dim,bias=False)
-    self.act = nn.SiLU()
-    
-  def forward(self,x):
-    x1 = self.act(self.w1(x)) 
-    gated_value = self.w3(x)
-    x = x1 * gated_value #swiglu
-    return self.w2(x)
+from .gqa_sa import GQA_SA
+from common import Frequencies, RMSE_NORM, FFN_SW
   
 class Attention_Block(nn.Module):
   def __init__(self,config):
     super().__init__()
     
     self.atten = GQA_SA(config["num_heads_q"],  config["num_heads_kv"], config["hidden_dims"])
-    self.norm1 = RMSE_NORM(config["hidden_dims"], config["eps"])
-    self.norm2 = RMSE_NORM(config["hidden_dims"], config["eps"])
-    self.ffn = FFN(config["hidden_dims"], config["ffn_multiplier"])
+    self.atten_norm = RMSE_NORM(config["hidden_dims"], config["eps"])
+    self.ffn_norm = RMSE_NORM(config["hidden_dims"], config["eps"])
+    self.ffn = FFN_SW(config["hidden_dims"], config["intermediate_dim"])
     
-  def forward(self,x,freq,mask):
-    x = x + self.atten(self.norm1(x),freq,mask)
-    x = self.ffn(self.norm2(x))
+  def forward(self,x, freq, mask):
+    x = x + self.atten(self.atten_norm(x), freq, mask)
+    x = x + self.ffn(self.ffn_norm(x))
     return x
   
 class Mistral(nn.Module):
@@ -55,50 +24,39 @@ class Mistral(nn.Module):
     self.blocks = nn.ModuleList([Attention_Block(config) for _ in range(config["num_blocks"])])
     self.embedding = nn.Embedding(config["vocab_size"],config["hidden_dims"])
     self.final_layer = nn.Linear(config["hidden_dims"],config["vocab_size"],bias=False)
-    self.rmse_norm = RMSE_NORM(config["hidden_dims"],config["eps"])
+    self.model_norm = RMSE_NORM(config["hidden_dims"],config["eps"])
     
-    self.rotatory_freq = Frequencies(config["seq_length"],config["head_dim"])
-    self.window_size = config["window_size"]
+    self.freq = Frequencies(config["seq_length"], config["head_dim"])
+    self.w = config["window_size"]
     
-  def forward(self,tokens):
-    x = self.embedding(tokens)
-    _,s,_ = x.shape
+  def forward(self, x):
+    s = x.shape[1]
     
-    complex_frequncies = self.rotatory_freq.complex_freq
-    i = torch.arange(0,s).view(s,1)
-    j = torch.arange(0,s).view(1,s)
-    mask = (i>=j) & (j >= i - self.window_size + 1)
+    x = self.embedding(x)
+    
+    mask = torch.full((s,s), -torch.inf)
+    casual_mask = torch.triu(torch.ones(s,s), diagonal=1)
+    window_mask = torch.tril(torch.ones(s,s), diagonal=-self.w)
+    final_mask = (casual_mask==1) | (window_mask ==1)
+    mask = mask.masked_fill(final_mask, -torch.inf).masked_fill(~final_mask, 0.0)
+    print(mask)
+    
     for block in self.blocks:
-      x = block(x,complex_frequncies,mask)
+      x = block(x, self.freq.complex_freq, mask)
     
-    x = self.rmse_norm(x)
+    x = self.model_norm(x)
     return self.final_layer(x)
       
-config = {
-    "model": {
-        "num_blocks": 2,
-        "hidden_dims": 256,
-        "num_heads_q": 8,
-        "num_heads_kv": 4,
-        "seq_length": 4096,
-        "ffn_multiplier": 4,
-        "vocab_size": 52000,
-        "eps": 1e-5,
-        "head_dim" : 32,
-        "window_size" : 4
-    }
-}
-
-# mis = Mistral(config["model"])
-# x = torch.randint(0,config["model"]["vocab_size"],size=(2,config["model"]["seq_length"]))
-# print(mis(x).shape)
-# print(mis)
-
+# config = {
+#     "num_blocks": 2,
+#     "hidden_dims": 256,
+#     "num_heads_q": 8,
+#     "num_heads_kv": 4,
+#     "seq_length": 4096,
+#     "intermediate_dim": 512,
+#     "vocab_size": 52000,
+#     "eps": 1e-5,
+#     "head_dim" : 32,
+#     "window_size" : 256
     
-    
-    
-    
-  
-    
-    
-    
+# }
